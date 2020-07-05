44a18f2cee782ee32aa02448fa09939a5a4f7144
anhanh1403 <66735089+anhanh1403@users.noreply.github.com>
2020-07-05 21:02:20 +0700

    Update Constants.java

--- Constants.java
+++ Constants.java
@@ -1,10 +1,10 @@
 package demo_Login;
 
 public class Constants {
-  public static String FACEBOOK_APP_ID = "";
-  public static String FACEBOOK_APP_SECRET = "";
-  public static String FACEBOOK_REDIRECT_URL = "";
-  public static String FACEBOOK_LINK_GET_TOKEN = "";
+  public static String FACEBOOK_APP_ID = "3183327778383252";
+  public static String FACEBOOK_APP_SECRET = "b88e5fcb9ed974d4287ca5f75610404f";
+  public static String FACEBOOK_REDIRECT_URL = "https://localhost:8443/demo_Login/login-fb";
+  public static String FACEBOOK_LINK_GET_TOKEN = "https://graph.facebook.com/oauth/access_token?client_id=%s&client_secret=%s&redirect_uri=%s&code=%s";
   
   public static String GOOGLE_CLIENT_ID = "450020186857-712ujs7enb5qfh1e7i2le6cga2kt90i4.apps.googleusercontent.com";
   public static String GOOGLE_CLIENT_SECRET = "db9oMIF4DcrWjH_MfCiQ7fVb";
